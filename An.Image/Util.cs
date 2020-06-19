b9d723dc11ce224b86941a5c5b8bdb3f4a330aa7
jianxingwu <jianxingwu@dj.cn>
2020-06-19 18:20:33 +0800

    添加 gif/png lib

---
+++ An.Image/Util.cs
@@ -0,0 +1,42 @@
+﻿using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Formats.Png;
+using SixLabors.ImageSharp.Processing;
+using System.Threading.Tasks;
+
+namespace An.Image
+{
+    public class Util
+    {
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="s"></param>
+        /// <param name="d"></param>
+        public static async Task ToPngAsync(string img, string save)
+        {
+            using var image = SixLabors.ImageSharp.Image.Load(img);
+            //image.Mutate(c => c.Resize(width, height).GaussianSharpen());
+            await image.SaveAsync(save, new PngEncoder());
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="img"></param>
+        /// <param name="save"></param>
+        /// <param name="width"></param>
+        /// <param name="height"></param>
+        /// <returns></returns>
+        public static async Task ToPngAsync(string img, string save, int width, int height)
+        {
+            using var image = SixLabors.ImageSharp.Image.Load(img);
+            image.Mutate(c => c.Resize(width, height).GaussianSharpen());
+            await image.SaveAsync(save, new PngEncoder());
+        }
+
+
+
+
+    }
+}
---
+++ An.Test/AnImageTest.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+
+namespace An.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            An.Image.Util.ToPngAsync(@"C:\Users\jxw\Desktop\gifŒ Ã‚\”„\002_00000.png");
+            Assert.Pass();
+        }
+    }
+}
